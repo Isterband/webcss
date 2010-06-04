bacf5e75f1c9f032b4cb2ac5e90e7203337cc3c1
Isterband <isterbandfb@gmail.com>
2010-06-04 10:46:00 +0200

    Edited README yet again

---
+++ demo.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<title>Webcss - Testpage</title>
+<link href="css/main.css" rel="stylesheet" type="text/css" name="webcss" />
+<script type="text/javascript" src="webcss/webcss.main.js"></script>
+</head>
+<body>
+<h1>This is webcss demopage!</h1>
+<p>Edit the css to your heart's content :)</p>
+
+<p>Testform, it won't do anything :O</p>
+<form action="" method="">
+	<label>Username: </label><input type="text" name="majs" /><br />
+	<label>Password: </label><input type="password" name="majs2" /><br />
+	<input type="submit" value="WOHO!" />
+</form>
+</body>
+</html>
