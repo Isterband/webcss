b64799dcc190eaaf5ac08cafeb5eabc066c79b73
Isterband <isterbandfb@gmail.com>
2010-06-04 04:42:09 +0200

    Added and stylized the webcss console

--- webcss/js/webcss.jquery.functions.js
+++ webcss/js/webcss.jquery.functions.js
@@ -2,6 +2,14 @@ $(document).ready(function() {
 
 	function spawn() {
 		
+		$("head").append("<link href=\"webcss/webcss.css\" rel=\"stylesheet\" type=\"text/css\" />");
+		
+		$("body").prepend("<div id=\"webcss_console\"></div>");
+		$("#webcss_console").append("<div id=\"webcss_console_head\"></div>");
+		$("#webcss_console").append("<div id=\"console_container\"><textarea id=\"webcss_textarea\"></textarea></div>");
+		
 	}
+	
+	spawn();
 
 });
