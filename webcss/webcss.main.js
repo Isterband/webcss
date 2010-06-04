9f48ecd75b90ef018f2e3c906ae653c74b77e9d8
Isterband <isterbandfb@gmail.com>
2010-06-04 02:47:55 +0200

    Will now load jQuery if it is not already loaded

---
+++ webcss/webcss.main.js
@@ -0,0 +1,16 @@
+if(typeof jQuery == 'undefined') {
+	
+	var script = document.createElement("script");
+	script.setAttribute("src", "webcss/js/jquery-1.4.2.min.js");
+	script.setAttribute("type", "text/javascript");
+	document.getElementsByTagName("head")[0].appendChild(script);
+	script = document.createElement("script");
+	script.setAttribute("src", "webcss/js/webcss.jquery.functions.js");
+	script.setAttribute("type", "text/javascript");
+	document.getElementsByTagName("head")[0].appendChild(script);
+	
+} else {
+	
+	$("head").append("<script type=\"text/javascript\" src=\"webcss/js/webcss.jquery.functions.js\">");
+	
+}
