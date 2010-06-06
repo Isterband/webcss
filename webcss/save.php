030d326e9358c3c4ded8f09d6a57e28c9d3a7f43
Isterband <isterbandfb@gmail.com>
2010-06-06 07:13:54 +0200

    Just did some minor changes

--- webcss/save.php
+++ webcss/save.php
@@ -1,15 +1,24 @@
 <?php
 	$data = $_POST['data'];
-	preg_match('/([a-zA-Z0-9\/\.]+\.css)/', $_POST['file'], $matches);
-	$filePath = realpath("../" . $matches[1]);
+	define("WEBROOT_PATH", realpath(".."));
+	preg_match('#^((?:(?:\w:\\\\)|(?:\w+://))?[\w/\\\\]+\.css)#', $_POST['file'], $matches);
+	if(!preg_match('#^((?:\w:\\\\)|(?:\w+://)[\w/\\\\]+\.css)#', $matches[1])) {
+		$filePath = realpath("../" . $matches[1]);
+	} else {
+		// $filePath = $matches[1];
+		echo "Filepath is not relative to the webroot";
+		exit;
+	}
 	
-	if(!empty($filePath)) {
+	if(empty($filePath)) {
+		echo "Filepath is empty";
+	} elseif(!file_exists($filePath)) {
+		echo "File doesn't exist: " . $filePath;
+	} else {
 		if(file_put_contents($filePath, $data)) {
 			echo "success";
+		} else {
+			echo "Unexpected error occured";
 		}
-	} else {
-		
-		echo $_POST['file'];
-		
 	}
 ?>
